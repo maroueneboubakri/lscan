ea6bdb62df6e9cfe1943ebe12994aff759b72cb6
Marouene Boubakri <maroueneboubakri@hotmail.com>
2016-06-24 15:03:03 +0200

    refact

--- src/bin-glib2-2.44.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include <glib.h>
-int main(int argc, char** argv) {
- GList* list = NULL;
- printf("lscan binary test file statically compiled with glib2-2.44\n");
- list = g_list_append(list, "Hello world!");
- printf("The first item is '%s'\n", g_list_first(list)->data);
- return 0;
-}
