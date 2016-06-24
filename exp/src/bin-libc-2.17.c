a4e9c15b7dfaec42a87331a7be31805ad2513383
root <root@debian-armhf>
2016-06-24 12:27:54 +0000

    experimentation for arm arch

---
+++ exp/src/bin-libc-2.17.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+
+printf("lscan binary test file statically linked with libc-2.17\n");
+return 0;
+}
---
+++ exp/src/bin-libm-2.22.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+
+printf("lscan binary test file statically linked with libm-2.22\n");
+
+double result, x = 4.34;
+  result = exp(x);
+  printf("'e' raised to the power of %.2lf (e^%.2lf) = %.2lf\n", x, x, result);
+
+return 0;
+}
